c099eb7299488cb6bd7e2674d7fde08322980a61
Vivek Gandharkar <vivekgandharkar@outlook.com>
2021-09-26 15:26:48 +0530

    feat(auth): add authTypes

---
+++ src/contexts/auth/authTypes.js
@@ -0,0 +1,5 @@
+export const LOGIN = 'LOGIN';
+export const LOGUT = 'LOGUT';
+export const REGISTER = 'REGISTER';
+export const FETCH_USER_DATA = 'FETCH_USER_DATA';
+export const SET_TOKEN = 'SET_TOKEN';
