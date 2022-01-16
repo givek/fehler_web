cb44d86a6e4500aebf8602a5eabb0caedada2fee
Vivek Gandharkar <vivekgandharkar@outlook.com>
2022-01-16 22:31:22 +0530

    feat(projects): add CreateProjectModal

---
+++ src/components/modals/CreateProjectModal.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalContent,
+  ModalOverlay,
+  ModalFooter,
+} from '@chakra-ui/modal';
+import { Button } from '@chakra-ui/button';
+import { Formik, Form } from 'formik';
+import { FormikControl } from '../FormikControl';
+import { Stack } from '@chakra-ui/layout';
+
+export const CreateProjectModal = props => {
+  return (
+    <Modal isOpen={props.isOpen} onClose={props.onClose}>
+      <ModalOverlay />
+      <Formik>
+        <Form>
+          <ModalContent>
+            <ModalHeader fontWeight="medium">Create a new project.</ModalHeader>
+            <ModalBody>
+              <Stack spacing={4}>
+                <FormikControl
+                  control="input"
+                  name="project_name"
+                  label="Project Name"
+                  type="text"
+                  size="sm"
+                />
+                <FormikControl
+                  control="textarea"
+                  name="project_description"
+                  label="Description"
+                  size="sm"
+                />
+              </Stack>
+            </ModalBody>
+            <ModalFooter>
+              <Button size="sm" variant="ghost" mr={3} onClick={props.onClose}>
+                Close
+              </Button>
+              <Button size="sm" colorScheme="blue">
+                Create
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Form>
+      </Formik>
+    </Modal>
+  );
+};
