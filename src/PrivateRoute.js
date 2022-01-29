443b4b47e4b8b6e7daa058be821e5368ad81673e
Vivek Gandharkar <vivekgandharkar@outlook.com>
2022-01-29 15:52:20 +0530

    refactor: PrivateRoute component
    - if isLoading, show loading spinner.
    - if not isAuthenticated, redirect to Login page.

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,21 +1,28 @@
+import { Spinner } from '@chakra-ui/react';
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import { useAuth } from './contexts/auth/authContext';
 
 export const PrivateRoute = ({ component: Component, ...rest }) => {
+  const { userData } = useAuth();
+  const { isAuthenticated, isLoading } = userData;
+
   return (
     <Route
       {...rest}
-      render={props => {
-        if (window.localStorage.getItem('userToken')) {
-          return <Component {...props} />;
-        } else {
-          return (
+      render={props =>
+        !isLoading ? (
+          isAuthenticated ? (
+            <Component {...props} />
+          ) : (
             <Redirect
               to={{ pathname: '/login', state: { from: props.location } }}
             />
-          );
-        }
-      }}
+          )
+        ) : (
+          <Spinner />
+        )
+      }
     />
   );
 };
