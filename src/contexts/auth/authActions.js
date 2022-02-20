96cbf4dd531f46529095708ae63e951040387f20
Vivek Gandharkar <vivekgandharkar@outlook.com>
2022-02-20 20:18:55 +0530

    fix: AuthProvider login issue

--- src/contexts/auth/authActions.js
+++ src/contexts/auth/authActions.js
@@ -1,9 +1,9 @@
-import fehlerApi from '../../utils/fehlerApi';
+import axios from 'axios';
 import { SET_TOKEN } from './authTypes';
 
 export async function login(data, dispatchToken) {
   try {
-    const response = await fehlerApi.post(`token`, data);
+    const response = await axios.post(`http://127.0.0.1:8000/api/token/`, data);
 
     if (response.status === 200) {
       dispatchToken({ type: SET_TOKEN, token: response.data.token });
@@ -27,7 +27,10 @@ export async function login(data, dispatchToken) {
 
 export async function register(data, dispatchToken) {
   try {
-    const response = await fehlerApi.post(`register`, data);
+    const response = await axios.post(
+      `http://127.0.0.1:8000/api/register/`,
+      data
+    );
 
     if (response.status === 200) {
       dispatchToken({ type: SET_TOKEN, token: response.data.token });
@@ -39,11 +42,12 @@ export async function register(data, dispatchToken) {
     }
   } catch (error) {
     if (error.response) {
-      return {
-        ok: false,
-        successMessage: null,
-        errors: error.response.data.errors,
-      };
+      console.log(error.response.data);
+      // return {
+      //   ok: false,
+      //   successMessage: null,
+      //   errors: error.response.data.errors,
+      // };
     }
     alert(error);
   }
