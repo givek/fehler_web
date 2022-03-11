b393fcb5dca72fb90e35cfc6a79cbc908487acb6
Vivek Gandharkar <vivekgandharkar@outlook.com>
2022-03-11 13:29:01 +0530

    fix(projects): create project

--- src/components/modals/CreateProjectModal.js
+++ src/components/modals/CreateProjectModal.js
@@ -11,16 +11,45 @@ import { Button } from '@chakra-ui/button';
 import { Formik, Form } from 'formik';
 import { FormikControl } from '../FormikControl';
 import { Stack } from '@chakra-ui/layout';
+import useAuthFehlerApi from '../../hooks/useAuthFehlerApi';
 
 const initialValues = {
-  name: '',
+  project_name: '',
+  project_description: '',
 };
 
 export const CreateProjectModal = props => {
+  const authFehlerApi = useAuthFehlerApi();
+  const onSubmit = async (data = {}, { setErrors }) => {
+    console.log(`form data`, data);
+
+    try {
+      // TODO: current space id is hardcoded, make it variable.
+      const response = await authFehlerApi.post(`create-project/`, {
+        name: data.project_name,
+        // owner: props.user.email,
+        space: 1,
+      });
+
+      console.log(response);
+
+      if (response) {
+        props.setProjects(response.data);
+      }
+    } catch (error) {
+      // TODO: handle errors
+      if (error.response) {
+        console.log(error.response);
+        console.log(error.response.status);
+        console.log(error.response.data.name);
+      }
+      alert(error);
+    }
+  };
   return (
     <Modal isOpen={props.isOpen} onClose={props.onClose}>
       <ModalOverlay />
-      <Formik initialValues={initialValues}>
+      <Formik onSubmit={onSubmit} initialValues={initialValues}>
         <Form>
           <ModalContent>
             <ModalHeader fontWeight="medium">Create a new project.</ModalHeader>
@@ -45,7 +74,7 @@ export const CreateProjectModal = props => {
               <Button size="sm" variant="ghost" mr={3} onClick={props.onClose}>
                 Close
               </Button>
-              <Button size="sm" colorScheme="blue">
+              <Button type="submit" size="sm" colorScheme="blue">
                 Create
               </Button>
             </ModalFooter>
