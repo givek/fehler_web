5ee4929beff8e59ae2e8f6d0c90cfee5d94aea99
Vivek Gandharkar <vivekgandharkar@outlook.com>
2022-03-31 19:46:40 +0530

    feat(CreateIssueModal): add onSubmit handler

--- src/components/modals/CreateIssueModal.js
+++ src/components/modals/CreateIssueModal.js
@@ -8,34 +8,81 @@ import {
   ModalFooter,
 } from '@chakra-ui/modal';
 import { Button } from '@chakra-ui/button';
-import { Formik, Form } from 'formik';
+import { Formik, Form, Field } from 'formik';
 import { FormikControl } from '../FormikControl';
 import { Stack } from '@chakra-ui/layout';
+import useAuthFehlerApi from '../../hooks/useAuthFehlerApi';
+import { useQueryClient } from 'react-query';
 
 const issueTypeOptions = [{ key: 'Frontend', value: 'frontend' }];
-const projectNameOptions = [
-  { key: 'Bacon Project', value: 'bacon_project' },
-  { key: 'Tuna Project', value: 'tuna_project' },
-];
+
 const issueAssigneeList = [{ key: 'Jon Doe', value: 'jon@email.com' }];
 const issueLabels = [{ key: 'Bug', value: 'bug' }];
-const issueReporter = [{ key: 'Vivek Gandharkar', value: 'vivekg@email.com' }];
 
-const initialValues = {
-  task_name: '',
-  project: '',
-  issue_description: '',
-  issue_type: '',
-  issue_assignee: '',
-  issue_reporter: '',
-  date_due: '',
-};
-
+// When `Kanban` page is directly accessed with url, state object (state: { id: project.id, projectName: project.name }) is not passed through router.
 export const CreateIssueModal = props => {
+  console.log(props.user.email);
+
+  console.log('project', props.projects);
+
+  const projectNameOptions = props.projects.map(project => ({
+    key: project.name,
+    value: project.id,
+  }));
+
+  console.log(projectNameOptions);
+
+  const issueReporter = [
+    {
+      key: `${props.user.first_name} ${props.user.last_name}`,
+      value: props.user.id,
+    },
+  ];
+
+  const initialValues = {
+    name: '',
+    project: 0,
+    description: '',
+    // issue_type: '',
+    // issue_assignee: '',
+    reporter: props.user.id,
+    date_due: '',
+  };
+
+  const authFehlerApi = useAuthFehlerApi();
+  const queryClient = useQueryClient();
+
+  const onSubmit = async (data = {}, { setErrors }) => {
+    console.log(`form data`, data);
+
+    try {
+      const response = await authFehlerApi.post(
+        `MeowSpace/Tuna/create_task/`,
+        data
+      );
+
+      console.log(response);
+
+      if (response) {
+        // props.setProjects(response.data);
+        queryClient.invalidateQueries('tasks');
+        queryClient.invalidateQueries('MeowSpace', 'Tuna', 'columns');
+      }
+    } catch (error) {
+      // TODO: handle errors
+      if (error.response) {
+        console.log(error.response);
+        console.log(error.response.status);
+        console.log(error.response.data.name);
+      }
+      alert(error);
+    }
+  };
+
   return (
     <Modal size="xl" isOpen={props.isOpen} onClose={props.onClose}>
       <ModalOverlay />
-      <Formik initialValues={initialValues}>
+      <Formik initialValues={initialValues} onSubmit={onSubmit}>
         <Form>
           <ModalContent>
             <ModalHeader fontWeight="medium">Create a new issue</ModalHeader>
@@ -43,25 +90,25 @@ export const CreateIssueModal = props => {
               <Stack spacing={4}>
                 <FormikControl
                   control="input"
-                  name="task_name"
+                  name="name"
                   label="Name"
                   size="sm"
                 />
-                {/* <FormikControl
+                <FormikControl
                   control="select"
                   name="project"
                   label="Project"
                   options={projectNameOptions}
                   size="sm"
-                /> */}
+                />
                 <FormikControl
                   control="textarea"
-                  name="issue_description"
+                  name="description"
                   label="Description"
                   size="sm"
                 />
 
-                <FormikControl
+                {/* <FormikControl
                   control="select"
                   name="issue_type"
                   label="Issue Type"
@@ -82,15 +129,16 @@ export const CreateIssueModal = props => {
                   label="Labels"
                   options={issueLabels}
                   size="sm"
-                />
+                /> */}
                 <FormikControl
                   control="select"
-                  name="issue_reporter"
+                  name="reporter"
                   label="Reporter"
                   options={issueReporter}
                   disabled="disabled"
                   size="sm"
                 />
+
                 <FormikControl
                   control="date"
                   name="date_due"
@@ -103,7 +151,7 @@ export const CreateIssueModal = props => {
               <Button size="sm" variant="ghost" mr={3} onClick={props.onClose}>
                 Close
               </Button>
-              <Button size="sm" colorScheme="blue">
+              <Button type="submit" size="sm" colorScheme="blue">
                 Create
               </Button>
             </ModalFooter>
