461e218db917c6647eed5c6cf3f76ab78d6dc770
Vivek Gandharkar <vivekgandharkar@outlook.com>
2022-04-16 21:14:59 +0530

    fix: create issue from people page

--- src/pages/SpaceMembers.js
+++ src/pages/SpaceMembers.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Avatar, Box, Stack, Text, Wrap, WrapItem } from '@chakra-ui/react';
+import {
+  Avatar,
+  Box,
+  Stack,
+  Text,
+  useDisclosure,
+  Wrap,
+  WrapItem,
+} from '@chakra-ui/react';
 import { Navbar } from '../components/Navbar';
 import People from '../components/People';
 import { useQuery } from 'react-query';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { useAuth } from '../contexts/auth/authContext';
+import useAuthFehlerApi from '../hooks/useAuthFehlerApi';
+import { CreateIssueModal } from '../components/modals/CreateIssueModal';
 
 function fetchSpaceMembers(spaceName, token) {
   return axios.get(`http://127.0.0.1:8000/api/${spaceName}/space-members/`, {
@@ -14,14 +24,45 @@ function fetchSpaceMembers(spaceName, token) {
 }
 function SpaceMembers() {
   const params = useParams();
+  const createIssueModalDisclosure = useDisclosure();
+
+  const [projects, setProjects] = React.useState([]);
 
   const { userData } = useAuth();
   const user = userData.currentUser;
+
+  const authFehlerApi = useAuthFehlerApi();
 
   const userToken = localStorage.getItem('userToken');
   const spaceMembersQuery = useQuery(['space-members', params.spaceName], () =>
     fetchSpaceMembers(params.spaceName, userToken)
   );
+
+  React.useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const response = await authFehlerApi.get(
+          `${params.spaceName}/projects`
+        );
+
+        console.log(response);
+
+        if (response) {
+          console.log(response.data);
+          setProjects(response.data);
+        }
+      } catch (error) {
+        // TODO: handle errors
+        if (error.response) {
+          console.log(error.response);
+          console.log(error.response.status);
+          console.log(error.response.data.name);
+        }
+        alert(error);
+      }
+    };
+    fetchProjects();
+  }, [authFehlerApi]);
 
   if (!spaceMembersQuery.data) {
     return <div>Loading...</div>;
@@ -32,29 +73,31 @@ function SpaceMembers() {
       <Navbar
         spaceName={params.spaceName}
         user={user}
-        // onOpen={createIssueModalDisclosure.onOpen}
+        onOpen={createIssueModalDisclosure.onOpen}
       />
-      {/* <CreateIssueModal
+      <CreateIssueModal
         user={user}
         projects={projects}
-        spaceName={spaceName}
+        spaceName={params.spaceName}
         isOpen={createIssueModalDisclosure.isOpen}
         onOpen={createIssueModalDisclosure.onOpen}
         onClose={createIssueModalDisclosure.onClose}
-      /> */}
+      />
 
       <Box px="110px" py="48px">
         <Wrap>
           <WrapItem>
             <Stack direction="row" alignItems="center" spacing="4">
-              {/* <Avatar size="lg" bg="#E87D65" color="#fff" name={spaceName} /> */}
-              <Avatar size="lg" bg="#E87D65" color="#fff" name="Tuna" />
-              {/* <Text fontWeight="semibold">{spaceName}</Text> */}
-              <Text fontWeight="semibold">Tuna</Text>
+              <Avatar
+                size="lg"
+                bg="#E87D65"
+                color="#fff"
+                name={params.spaceName}
+              />
+              <Text fontWeight="semibold">{params.spaceName}</Text>
             </Stack>
           </WrapItem>
         </Wrap>
-
         <People title="Space Members" people={spaceMembersQuery?.data?.data} />
       </Box>
     </Box>
